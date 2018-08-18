cd2ab304a60cc6fe452d121a65fa6f1dde6ac8d9
Dennis Vriend <dnvriend@gmail.com>
2018-08-18 09:06:37 +0200

    boto3

--- buckets/buckets.py
+++ buckets/buckets.py
@@ -1,10 +1,24 @@
+from typing import Optional, Sequence
 import click
+import boto3
+from txtable.table import TextTable
 
+client = boto3.client('s3')
 
 @click.command()
-def main():
+def main() -> None:
     """Bucket, your interface to S3"""
-    print("Hello, I am Buckets")
+    rows = []
+    rows.append(['BucketName', 'CreationDate'])
+    for bucket in client.list_buckets()['Buckets']:
+        rows.append([bucket['Name'], bucket['CreationDate'].__str__()])
+
+    print(render_table(rows))
+
+
+def render_table(rows: Optional[Sequence]) -> str:
+    """Creates an ASCII table and returns the string result"""
+    return TextTable(rows=rows, formatter=None).__str__()
 
 
 # only necessary for testing
